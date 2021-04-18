207f9f6098af659cc0d34c013c42360d61fc1c4b
Sawami Tao <macaron.3031@icloud.com>
2021-04-18 21:25:33 +0900

    はじめてのコミット

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by 田尾　早和美 on 2021/04/18.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
